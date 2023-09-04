efb261ee29d2f7813d9188aa797abd3da5320816
Bokyeong <bo_bo_@kakao.com>
2023-09-04 14:51:53 +0900

    install cocoapods

---
+++ bilboard/bilboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bilboard
+//
+//  Created by 남보경 on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

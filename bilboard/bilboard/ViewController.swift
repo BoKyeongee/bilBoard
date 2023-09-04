4ff4e4b906c8d607e3fa63bd3df18ba680f9ffb7
Bokyeong <bo_bo_@kakao.com>
2023-09-04 14:53:58 +0900

    feat: Add NMapsMap api id

--- bilboard/bilboard/ViewController.swift
+++ bilboard/bilboard/ViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import NMapsMap
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let mapView = NMFMapView(frame: self.view.bounds)
+             mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+             self.view.addSubview(mapView)
     }
 
 
